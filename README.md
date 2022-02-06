de5928822df1b1a8ac464b61b4b74e4467b42d88
Daniel <ddoherty6@gmail.com>
2022-02-05 16:59:35 -0800

    added USE to schema, added vid links to readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# 13-E-commerce-Back-End
+
+vid 1
+https://watch.screencastify.com/v/iQZNqbIVwkbeUvPCM9kH
+
+vid 2
+https://watch.screencastify.com/v/tb1bBzY57aUEVUBRatkl
--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,6 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- adding this is in, automates USE database
+USE DATABASE ecommerce_db;
+
